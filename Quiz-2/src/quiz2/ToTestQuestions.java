8fca126751223d0294a6ec71c2f7033f57dba0b4
Sam <Sam@128.4.138.121>
2017-02-24 20:11:21 -0500

    Solution in quiz2

---
+++ Quiz-2/src/quiz2/ToTestQuestions.java
@@ -0,0 +1,11 @@
+package quiz2;
+
+public class ToTestQuestions {
+	public static void main(String[] args){
+		int count = 5;
+		double amount = 45.56;
+		System.out.printf("count is %d and amount is %f", count, amount);
+		
+	}
+
+}
--- Quiz-2/src/quiz2/quiz2.java
+++ Quiz-2/src/quiz2/quiz2.java
@@ -1,6 +1,24 @@
 package quiz2;
 
+import java.util.Scanner;
 public class quiz2 {
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter the tuition cost: ");
+		double tuition = input.nextDouble();
+		
+		double percentage;
+		double sum = 0;
+		
+		for(int year = 0; year < 3; year++){
+			System.out.print("Enter the percentage increase: ");
+			percentage = input.nextDouble();
+			sum += tuition;
+			tuition += tuition*percentage;
+		}
+		System.out.printf("The cost for attending school for four years is " + "%.2f", sum);
+	}
 	
 
 }
